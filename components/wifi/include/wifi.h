0b558b6a92fa7fcbc407606db22ced63def4152a
Kluoke <1125919032@qq.com>
2026-07-13 11:55:24 +0800

    Add Wi-Fi scan and status interfaces

--- components/wifi/include/wifi.h
+++ components/wifi/include/wifi.h
@@ -2,9 +2,34 @@
 #define __WIFI_H__
 
 #include <stdbool.h>
+#include <stdint.h>
+#include "esp_err.h"
+
+#define WIFI_SCAN_MAX_APS 20
+
+typedef struct {
+    char ssid[33];
+    int8_t rssi;
+    uint8_t authmode;
+} wifi_scan_result_t;
+
+typedef void (*wifi_status_callback_t)(const char *status);
 
 void wifi_init(void);
 bool wifi_is_provisioned(void);
 void wifi_save_credentials_and_connect(const char *ssid, const char *password);
+
+/**
+ * @brief Scan nearby access points synchronously.
+ *
+ * @param results Caller-provided array containing at least WIFI_SCAN_MAX_APS entries.
+ * @param count In: capacity of results. Out: number of records returned.
+ */
+esp_err_t wifi_scan_networks(wifi_scan_result_t *results, uint16_t *count);
+
+/**
+ * @brief Register a callback for Wi-Fi provisioning status changes.
+ */
+void wifi_set_status_callback(wifi_status_callback_t callback);
 
 #endif
